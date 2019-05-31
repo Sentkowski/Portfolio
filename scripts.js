4efc422b80777ecd12c499ed89093bbc0c9089e1
Szymon <39162021+Sentkowski@users.noreply.github.com>
2019-05-31 18:46:17 +0200

    Added reveal animations and svg paintings

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+import * as vivus from "./vivus.js"
+
 adjustViewport();
 
 window.addEventListener("load", function() {
@@ -5,15 +7,13 @@ window.addEventListener("load", function() {
     const navHandler = navManager();
     activateNavLinks();
 
-    createDesktopSkillsObserver();
-    createDesktopProjectsObserver();
+    new Vivus('my-pic', {duration: 200, type: 'sync', animTimingFunction: Vivus.EASE, start: 'autostart'});
+    document.querySelector("#my-pic").style.opacity = "0.75";
 
-    if (!window.matchMedia("(min-width: 650px)").matches) {
-        createMobileSkillsObserver();
-        createMobileProjectsObserver()
-    }
-
-    intersectionObserverFallback();
+    new Vivus('me-pointing-about-right', {duration: 200, type: 'sync', animTimingFunction: Vivus.EASE});
+    new Vivus('me-pointing-about-left', {duration: 200, type: 'sync', animTimingFunction: Vivus.EASE});
+    new Vivus(`stars-left`, {duration: 50, type: 'oneByOne', animTimingFunction: Vivus.EASE}, () => keepDrawingStars('left'));
+    new Vivus(`stars-right`, {duration: 50, type: 'oneByOne', animTimingFunction: Vivus.EASE}, () => keepDrawingStars('right'));
     
     document.querySelector(".welcome-nav-arrow").addEventListener('click', navHandler);
     window.addEventListener('scroll', navHandler);
@@ -71,106 +71,21 @@ function handleInternalLink(evt) {
     evt.preventDefault();
 }
 
-function createDesktopSkillsObserver() {
-    const options = {
-        root: null,
-        rootMargin: "0px",
-        threshold: [0, 0.4]
-    };
-
-    const observer = new IntersectionObserver(handleDesktopSkillsIntersect, options);
-    observer.observe(document.querySelector(".skills-section"));
-}
-
-function handleDesktopSkillsIntersect(entries, observer) {
-    entries.forEach(entry => {
-        if (entry.intersectionRatio >= 0.35) {
-            document.querySelector(".skills-covering-rect-one").style.transform = "translateX(100%) scaleX(0.5)";
-            document.querySelector(".skills-covering-rect-two").style.transform = "translateX(-100%) scaleX(0.5)";
-            setTimeout(function() {
-                document.querySelector(".skills-covering-rect-one").style.transform = "translateX(100%) scaleX(0.5) scaleY(2.1)";
-                document.querySelector(".skills-covering-rect-two").style.transform = "translateX(-100%) scaleX(0.5) scaleY(2.1)";
-            }, 300);
-            observer.unobserve(entry.target);
-        }
-    });
-}
-
-function createDesktopProjectsObserver() {
-    const options = {
-        root: null,
-        rootMargin: "0px",
-        threshold: [0, 0.1]
-    };
-
-    const observer = new IntersectionObserver(handleDesktopProjectsIntersect, options);
-    observer.observe(document.querySelector(".projects-section"));
-}
-
-function handleDesktopProjectsIntersect(entries, observer) {
-    entries.forEach(entry => {
-        if (entry.intersectionRatio >= 0.1) {
-            document.querySelector(".projects-covering-rect-one").style.transform = "translateY(100%)";
-            document.querySelector(".projects-covering-rect-two").style.transform = "translateY(-100%)";
-            setTimeout(function() {
-                document.querySelector(".projects-covering-rect-one").style.transform = "translateY(100%) scaleX(1.5)";
-                document.querySelector(".projects-covering-rect-two").style.transform = "translateY(-100%) scaleX(1.5)";
-            }, 300);
-            observer.unobserve(entry.target);
-        }
-    });
-}
-
-function createMobileSkillsObserver() {
-    const options = {
-        root: null,
-        rootMargin: "0px",
-        threshold: [0.1, 0.2]
-    };
-
-    const observer = new IntersectionObserver(handleMobileSkillsIntersect, options);
-    document.querySelectorAll(".skill-list-item").forEach(skill => {observer.observe(skill)});
-}
-
-function handleMobileSkillsIntersect(entries, observer) {
-    entries.forEach(entry => {
-        if (entry.intersectionRatio > 0.15) {
-            const heading = entry.target.querySelector(".skill-list-heading-container");
-            heading.style.transform = `translateX(0px)`
-            observer.unobserve(entry.target);
-        }
-    });
-}
-
-function createMobileProjectsObserver() {
-    const options = {
-        root: null,
-        rootMargin: "0px",
-        threshold: [0.1, 0.2]
-    };
-
-    const observer = new IntersectionObserver(handleMobileProjectsIntersect, options);
-    document.querySelectorAll(".project").forEach(project => {observer.observe(project)});
-}
-
-function handleMobileProjectsIntersect(entries, observer) {
-    entries.forEach(entry => {
-        if (entry.intersectionRatio > 0.15) {
-            const heading = entry.target.querySelector(".project-name-container");
-            heading.style.transform = `translateX(0px)`
-            observer.unobserve(entry.target);
-        }
-    });
-}
-
-
-function intersectionObserverFallback() {
-    if (!('IntersectionObserver' in window) ||
-    !('IntersectionObserverEntry' in window) ||
-    !('intersectionRatio' in window.IntersectionObserverEntry.prototype)) {
-        document.querySelector(".projects-covering-rect-one").style.display = "none";
-        document.querySelector(".projects-covering-rect-two").style.display = "none";
-        document.querySelector(".skills-covering-rect-one").style.display = "none";
-        document.querySelector(".skills-covering-rect-two").style.display = "none";
+function keepDrawingStars(dir) {
+    const list = document.querySelector(`.stars-container-${dir}`);
+    const starsCurr = list.childElementCount;
+    const availableSpace = window.innerWidth - 150 - starsCurr * 300;
+    if (availableSpace > 0) {
+        const newStars = document.querySelector(`#stars-${dir}`).cloneNode(true);
+        newStars.id = `stars-${dir}-${availableSpace}`;
+        list.appendChild(newStars);
+        randomizeStars(newStars);
+        new Vivus(`stars-${dir}-${availableSpace}`, {duration: 50, type: 'oneByOne', animTimingFunction: Vivus.EASE}, () => keepDrawingStars(dir));
     }
+}
+
+function randomizeStars(stars) {
+    const spin = Math.floor(Math.random() * 360);
+    const scale = 0.8 + Math.random() * 0.4;
+    stars.style.transform = `scale(${scale}) rotate(${spin}deg)`;
 }
